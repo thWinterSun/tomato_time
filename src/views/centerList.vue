60ca33627bb1607471a88974ac2da34c0e95fb44
daijinxue <daijinxue@xdstar.net>
2019-05-20 14:49:37 +0800

    New todolist features

--- src/views/centerList.vue
+++ src/views/centerList.vue
@@ -1,30 +1,60 @@
 <template lang="html">
-  <div class="">
+  <div class="centerList">
     <Card :bordered="false" >
-      <Input  placeholder="在“收集箱”中添加任务，按回车键保存" icon="md-stopwatch"/>
+      <Input  placeholder="在“收集箱”中添加任务，按回车键保存" icon="md-stopwatch" v-on:keyup.enter="addTask" />
+        <!-- <Icon type="md-stopwatch" slot="extra" size="16"/>
+      </input> -->
     </Card>
     <br/>
-    <Card :bordered="false" >
-      <CellGroup>
-        <Cell title="任务1" extra="5月15日" >
-
-        </Cell>
-        <Cell title="任务2" extra="5月16日" >
-
-        </Cell>
-        <Cell title="任务3" extra="5月15日" >
-          
+    <!-- <Card :bordered="false" > -->
+      <CellGroup v-for="task in taskList">
+        <Cell :title="task.title" :extra="task.createDate" :label="task.tomatoNum">
+          <Radio slot="icon"></Radio>
+          <Icon type="md-arrow-dropright-circle" slot="icon" size="16"/>
         </Cell>
       </CellGroup>
-    </Card>
+    <!-- </Card> -->
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      taskList: [
+        {
+          title: '任务一',
+          createDate: '5月15日',
+          tomatoNum: '2个番茄'
+        },
+        {
+          title: '任务二',
+          createDate: '5月20日',
+          tomatoNum: '2个番茄'
+        },
+        {
+          title: '任务三',
+          createDate: '5月16日',
+          tomatoNum: '2个番茄'
+        }
+      ]
+    }
+  },
+  methods: {
+    addTask () {
+      console.log('增加任务');
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .ivu-cell-item .ivu-radio-wrapper {
+    margin-right: 0
+  }
+  .ivu-cell {
+    background: #fff;
+    margin-bottom: 5px;
+    border-radius: 5px;
+  }
 </style>
--- src/views/rightMenu.vue
+++ src/views/rightMenu.vue
@@ -3,6 +3,8 @@
     <Card :bordered="false" >
       <p slot="title">
         <Radio ></Radio>
+        <Icon type="md-arrow-dropright-circle" slot="icon" size="16"/>
+
         <!-- <Icon type="ios-arrow-dropright" /> -->
         <!-- <Icon type="ios-play-outline" /> -->
         任务名称
@@ -11,8 +13,13 @@
       <Icon type="ios-flag-outline" slot="extra" size='25'/>
 
       <CellGroup>
-        <Cell title="番茄数" extra="5月15日" >
+        <Cell title="番茄数" >
           <Icon type="md-stopwatch" slot="icon" size='20'/>
+          <Icon type="md-stopwatch" slot="extra" size='20'/>
+          <span slot="extra" style="font-size:15px"> 1 /</span>
+          <Icon type="md-stopwatch" slot="extra" size='20'/>
+          <span slot="extra" style="font-size:15px"> 2</span>
+
         </Cell>
         <Cell title="到期日" extra="5月16日 周四" >
           <Icon type="ios-calendar-outline" slot="icon" size='20'/>
@@ -27,6 +34,13 @@
         <Cell title="重复" extra="无" >
           <Icon type="ios-repeat" slot="icon" size='20'/>
         </Cell>
+      </CellGroup>
+      <CellGroup>
+        <Divider />
+        <Cell>
+          <Input prefix="md-add" placeholder="添加子任务" style="border:none" :bordered="false" />
+        </Cell>
+        <Divider />
       </CellGroup>
     </Card>
   </div>
